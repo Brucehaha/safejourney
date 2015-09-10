1a4dfcfefa09896ba6bed9b41440af177b2fd54f
Henning Li <henninglee2013@gmail.com>
2015-09-10 13:34:36 +1000

    feedbackformat

--- feedbackphp.php
+++ feedbackphp.php
@@ -15,13 +15,13 @@ if ($_POST["submit"]) {
 	}
     
     if ($_POST['email']!="" AND !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
-		$error.="Please enter a valid email address";
+		$error.="<div class='alert alert-danger'>Please enter a valid email address";
 	}
 	
 	if ($error) {	
 		$result='<strong>There were error(s) in your form:</strong>'.$error.'';
 	} else {
-		if (mail("safejourneytowork91@gmail.com", "Comment from website!", "Name: ".$_POST['name']."Email: ".$_POST['email']."Comment: ".$_POST['comment'])) {
+		if (mail("safejourneytowork91@gmail.com", "Comment from website!", "Name: ".$_POST['name'].'<br />'."Email: ".$_POST['email'].'<br />'."Comment: ".$_POST['comment'])) {
 			$result='<div class="alert alert-success"><strong>Thank you!</strong> I\'ll be in touch.</div>';
 		} else {
 			$result='<div class="alert alert-danger">Sorry, there was an error sending your message. Please try again later.</div>';
