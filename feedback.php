136aa8d3ccc90752c261b9e18138533924229078
Henning Lee <henninglee2013@gmail.com>
2015-09-18 13:38:42 +1000

    create outernal file of header and head 1

--- feedback.php
+++ feedback.php
@@ -1,4 +1,57 @@
-<?
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Your feedback</title>
+	<!-- call the outernal head file -->
+	<?php include("maps/head.html"); ?>
+</head>
+
+<body>
+    <? include("feedbackphp.php");?>
+	<!-- This is the code for the top of window to place the navigation button -->
+	<?php include("maps/header.html"); ?>
+	
+	<!-- Main form for feedback by users -->
+	<!-- Users should enter the name, email, some comments -->
+	<div class="container contentContainer" id="feedbackcontainer">
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3 emailForm">
+				<h1 style="color:grey;">We need your feedback for improvement</h1>
+				<p class="lead" style="color:grey;">We will contact you by email.</p>
+				<?php echo $result; ?>
+					
+				<form method="post">
+					<div class="form-group">
+						<label for="name" style="color:grey;">Name:</label><label style="color:red;">*</label>
+						<input type="text" name="name" class="form-control" placeholder="Your Name" />
+					</div>
+
+					<div class="form-group">
+						<label for="email" style="color:grey;">Email:</label><label style="color:red;">*</label>
+						<input type="email" name="email" class="form-control" placeholder="Your Email" />
+					</div>
+
+					<div class="form-group">
+						<label for="comment" style="color:grey;">Comment:</label><label style="color:red;">*</label>
+						<textarea class="form-control" name="comment" placeholder="Comments here, please..."></textarea>
+					</div>
+
+					<input type="submit" name="submit" class="btn btn-lg" value="Submit" />
+				</form>
+			</div>
+		</div>
+	</div>
+	
+	<!-- JavaScript -->
+	<!-- fit the background -->
+	<!-- make the background image fit for the size of screen -->
+	<script src="myjs.js" type="text/javascript"></script>
+	
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html><?
 	include("feedbackphp.php");
 ?>
 <!DOCTYPE html>
