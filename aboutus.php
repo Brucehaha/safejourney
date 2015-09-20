7ad616ee80d613f1417197746bc66d8f5820004b
Henning Lee <henninglee2013@gmail.com>
2015-09-20 14:36:56 +1000

    create outernal nar

--- aboutus.php
+++ aboutus.php
@@ -1,70 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Safe Journey To Work</title>
-	
-	<!-- logo -->
-	<link rel="shortcut icon" href="images/logo.png" />
-    <!-- css -->
-	<link rel="stylesheet" type="text/css" href="mystyle.css"> 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-	   
-	<!-- JQuery -->
-    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>   
+    <title>About Us</title>
+	<?php include("includes/head.html"); ?>  
   </head>
   
   <body>
-	<!-- This is the code for the top of window to place the navigation button -->
-    <div class="navbar navbar-default navbar-fix-top" id="topContainer">  
-        <div class="container">
-			<div class="navbar-header"> 
-				<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand navbar-left" href="http://www.safejourneytowork.tk/">SafeJourney</a>  
- 			</div>
- 			
-			<div class="collapse navbar-collapse">
-				<ul class="nav navbar-nav">
-					<li><a href="http://www.safejourneytowork.tk/" />Home</a></li>
-					<li class="dropdown">
-						<a id="drop1" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-						Search
-						<span class="caret"></span></a>
-						<ul class="dropdown-menu" aria-labelledby="drop1">
-							<li><a href="search.php">By Fine</a></li>
-							<li><a href="accidentreport.php">By Accident</a></li>
-							<li role="separator" class="divider"></li>
-							<li><a href="maptest.html">On Map</a></li>
-						</ul>
-					</li>
-					<li id="feedback"><a href="feedback.php">Feedback</a></li>						         
-					<li id="about"><a href="aboutus.php">About Us</a></li>	
-				</ul>
-				<form class="navbar-form navbar-right">
-					<div class="form-group">
-						<input type="email" placeholder="Email" class="form-control" />
-					</div>
-						
-					<div class="form-group">
-						<input type="password" placeholder="Password" class="form-control" />
-					</div>
-						
-					<button type="submit" class="btn" id="login">Log In</button>
-				</form>
-			</div>
-		</div>
-    </div>
+	<?php include("includes/header.html"); ?>
 	
 	<!-- Put the video about our system here -->
 	<div class="container contentContainer3" id="aboutvideo">
