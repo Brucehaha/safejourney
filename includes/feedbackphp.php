62de5e0f8710815c267edb852b578df350a5f1a5
Henninglee <henninglee2013@gmail.com>
2015-09-23 00:40:04 +1000

    22/09/2015 Version by Annie

--- includes/feedbackphp.php
+++ includes/feedbackphp.php
@@ -1,31 +1,42 @@
-<!-- This is php code for form input validation -->
-<!-- if user type nothing in the form, there will be displayed with error message or warning -->
-<?php 
-if ($_POST["submit"]) {
-	if (!$_POST['name']) {
-		$error="<div class='alert alert-danger'>Please enter your name</div>";
-	}
-	
-	if (!$_POST['email']) {
-		$error.="<div class='alert alert-danger'>Please enter your email address</div>";
-	}
-	
-	if (!$_POST['comment']) {
-		$error.="<div class='alert alert-danger'>Please enter a comment</div>";
-	}
-    
-    if ($_POST['email']!="" AND !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
-		$error.="<div class='alert alert-danger'>Please enter a valid email address";
-	}
-	
-	if ($error) {	
-		$result='<strong>There were error(s) in your form:</strong>'.$error.'';
-	} else {
-		if (mail("safejourneytowork91@gmail.com", "Comment from website!", "Name: ".$_POST['name'].'<br />'."Email: ".$_POST['email'].'<br />'."Comment: ".$_POST['comment'])) {
-			$result='<div class="alert alert-success"><strong>Thank you!</strong> I\'ll be in touch.</div>';
-		} else {
-			$result='<div class="alert alert-danger">Sorry, there was an error sending your message. Please try again later.</div>';
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  </head>
+  
+ <body>
+	<!-- This is php code for form input validation -->
+	<!-- if user type nothing in the form, there will be displayed with error message or warning -->
+	<?php 
+	if ($_POST["submit"]) {
+		if (!$_POST['name']) {
+			$error="<div class='alert alert-danger'>Please enter your name</div>";
 		}
-	}	
-}
-?>
+		
+		if (!$_POST['email']) {
+			$error.="<div class='alert alert-danger'>Please enter your email address</div>";
+		}
+		
+		if (!$_POST['comment']) {
+			$error.="<div class='alert alert-danger'>Please enter a comment</div>";
+		}
+		
+		if ($_POST['email']!="" AND !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
+			$error.="<div class='alert alert-danger'>Please enter a valid email address";
+		}
+		
+		if ($error) {	
+			$result='<strong>There were error(s) in your form:</strong>'.$error.'';
+		} else {
+			if (mail("safejourneytowork91@gmail.com", "Comment from website!", "Name: ".$_POST['name'].'<br />'."Email: ".$_POST['email'].'<br />'."Comment: ".$_POST['comment'])) {
+				$result='<div class="alert alert-success"><strong>Thank you!</strong> I\'ll be in touch.</div>';
+			} else {
+				$result='<div class="alert alert-danger">Sorry, there was an error sending your message. Please try again later.</div>';
+			}
+		}	
+	}
+	?>
+	
+  </body>
+</html>	
