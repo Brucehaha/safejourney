98ff538d68025df706de298cd3052a1c964e8794
Henninglee <henninglee2013@gmail.com>
2015-09-22 17:30:04 +1000

    22/09/2015 Version by Annie

--- accidentreport.php
+++ accidentreport.php
@@ -3,31 +3,17 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Safe Journey To Work</title>
-		<!-- logo -->
-		<link rel="shortcut icon" href="images/logo.png" />
-		<!-- css -->
-		<link rel="stylesheet" type="text/css" href="mystyle.css">
-		<!-- Bootstrap -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<!-- Morris chart style -->
 		<link rel="stylesheet" href="http://cdn.oesmith.co.uk/morris-0.5.1.css">
-   
-		<!--JQuery-->
-	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-		<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
-		
-		<style>
+		  <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+		<style>		    
 			<!-- Responsive tables firefox -->
 			@-moz-document url-prefix() {
 				fieldset { display: table-cell; }
 			}			
 	   </style>
-	   <!-- head style -->
+	   <!-- Head style -->
 	   <?php include("includes/head.html"); ?>  
 	   
 	   <script type="text/javascript">
@@ -39,44 +25,63 @@
   </head>
   
   <body>
+	<!--Navigation bar and style-->
 	<?php include("includes/header.html"); ?>
-    
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-sm-12" style="text-align:center;">
-				<p style="font-size:160%;color:grey;margin:30px 10px;">In recent years, an increasing number of accidents happened on the road for daily frequent commuters. The safety is always the most important issue all the world wide.</p>  
-				<!-- Dropdown list for user selection to display the relevant chart -->
-				<div>
-					<select id="searchList" name="searchList" style="width:150px;">
-						<option selected>Choose One...</option>
-						<option value="year">Year</option>
-						<option value="light">Light Condition</option>
-						<option value="daytime">Daytime</option>
-						<option value="weekday">Weekday</option>
-						<option value="speed">Speed Zone</option>
-					</select>
-					<select id="searchList2" name="searchList2" style="width:150px;">
-						<option value="none" selected>Select seasons...</option>
-						<option value="season1">Season 1</option>
-						<option value="season2">Season 2</option>
-						<option value="season3">Season 3</option>
-						<option value="season4">Season 4</option>
-						<option value="compare">Monthly Comparison</option>
-					</select>
-					<input type="submit" id="select" class="btn" name="searchBtn" value="Search" />
-				</div>
-				<hr>	
-			</div>
-		</div>
-		
-		<div class="row">
-			<!-- Display the graph report here -->
-			<div class="col-md-6 col-md-offset-3"> 					
-				<div id="resultpanel"></div>
-			</div>   
-		</div>
-	</div>		
 
+    <div class="container">
+		<div class="row">
+			<div class="col-md-2 left-side" >
+				<h4>Year</h4>
+				<ul style="list-style-type:none;">
+				  <li>2010</li>
+				  <li>2011</li>
+				  <li>2012</li>
+				  <li>2013</li>
+				  <li>2014</li>
+				</ul>
+				<hr>
+				<h4>Comparison</h4>
+				<ul style="list-style-type:none;">
+				<li><input type="submit" id="monthly" class="btn" name="monthlyBtn" value="Monthly" style="width:80px;height:40px;" disabled /></li><br />
+				<li><input type="submit" id="daily" class="btn" name="dailyBtn" value="Daily" style="width:80px;height:40px;" disabled /></li><br />
+				<li><input type="submit" id="time" class="btn" name="timeBtn" value="Time" style="width:80px;height:40px;" disabled /></li><br />
+			</div>
+			<div class="col-md-10 right-side">
+				<div style="text-align:center;">
+					<p style="font-size:160%;color:grey;margin:30px 10px;">In recent years, an increasing number of accidents happened on the road for daily frequent commuters. The safety is always the most important issue all the world wide.</p> 
+					<!-- Dropdown list for user selection to display the relevant chart -->
+					<div>
+						<label for="searchList">Search by</label>
+						<select id="searchList" name="searchList" style="width:150px;">
+							<option selected>Choose One...</option>
+							<option value="year">Year</option>
+							<option value="weekday">Daily</option>
+							<option value="daytime">24 Hours</option>
+							<option value="light">Light Condition</option>
+							<option value="speed">Speed Zone</option>
+						</select>
+						<select id="searchList2" name="searchList2" style="width:150px;">
+							<option value="none" selected>Select option...</option>
+							<option value="q1">Quarter 1</option>
+							<option value="q2">Quarter 2</option>
+							<option value="q3">Quarter 3</option>
+							<option value="q4">Quarter 4</option>
+							<option value="compare">Monthly Comparison</option>
+						</select>
+						<input type="submit" id="select" class="btn" name="searchBtn" value="Search" />
+					</div>	
+				</div>
+					
+				<div style="text-align:center;">
+					<!-- Display the graph report here -->
+					<div style="text-align:center;"> 					
+						<div id="resultpanel"></div>
+					</div>   
+				</div>
+			</div>	
+		</div>
+	</div>
+	
 	<!-- FOOTER -->
     <footer>
 		<p class="pull-right" style="margin-top:50px;margin-right:70px;"><a href="#">Back to top</a></p>
@@ -100,38 +105,38 @@
 		$("#select").click(function () {
 			//Show line chart based on the selection of "Year"
 			if ($("#searchList option:selected").val() == "year" && $("#searchList2 option:selected").val() == "none"){
-				$("#resultpanel").load("chart1.php");	
+				$("#resultpanel").load("accidentchartsphp/chart1.php");	
 			} 
-			else if ($("#searchList option:selected").val() == "year" && $("#searchList2 option:selected").val() == "season1") {
-				$("#resultpanel").load("chart2.php");
+			else if ($("#searchList option:selected").val() == "year" && $("#searchList2 option:selected").val() == "q1") {
+				$("#resultpanel").load("accidentchartsphp/chart2.php");
 			} 
-			else if ($("#searchList option:selected").val() == "year" && $("#searchList2 option:selected").val() == "season2") {
-				$("#resultpanel").load("chart3.php");	
+			else if ($("#searchList option:selected").val() == "year" && $("#searchList2 option:selected").val() == "q2") {
+				$("#resultpanel").load("accidentchartsphp/chart3.php");	
 			}
-			else if ($("#searchList option:selected").val() == "year" && $("#searchList2 option:selected").val() == "season3") {
-				$("#resultpanel").load("chart4.php");	
+			else if ($("#searchList option:selected").val() == "year" && $("#searchList2 option:selected").val() == "q3") {
+				$("#resultpanel").load("accidentchartsphp/chart4.php");	
 			}
-			else if ($("#searchList option:selected").val() == "year" && $("#searchList2 option:selected").val() == "season4") {
-				$("#resultpanel").load("chart5.php");
+			else if ($("#searchList option:selected").val() == "year" && $("#searchList2 option:selected").val() == "q4") {
+				$("#resultpanel").load("accidentchartsphp/chart5.php");
 			}
 			else if ($("#searchList option:selected").val() == "year" && $("#searchList2 option:selected").val() == "compare") {
-				$("#resultpanel").load("chart10.php");
+				$("#resultpanel").load("accidentchartsphp/chart10.php");
 			}
 			//Show bar chart based on the selection of "Light Condition"
 			else if ($("#searchList option:selected").val() == "light") {
-				$("#resultpanel").load("chart6.php");
+				$("#resultpanel").load("accidentchartsphp/chart6.php");
 			} 
 			//Show bar chart based on the selection of "Day Time": morning, afternoon, evening, night
 			else if ($("#searchList option:selected").val() == "daytime") {
-				$("#resultpanel").load("chart7.php");	
+				$("#resultpanel").load("accidentchartsphp/chart7.php");	
 			}
 			//Show bar chart based on the selection of "Weekday"
 			else if ($("#searchList option:selected").val() == "weekday") {
-				$("#resultpanel").load("chart8.php");	
+				$("#resultpanel").load("accidentchartsphp/chart8.php");	
 			}
 			//Show bar chart based on the selection of "Speed Zone"
 			else if ($("#searchList option:selected").val() == "speed") {
-				$("#resultpanel").load("chart9.php");	
+				$("#resultpanel").load("accidentchartsphp/chart9.php");	
 			}
 			//Display error message when user click the button without any selection from dropdown menu
 			else {
@@ -140,5 +145,7 @@
 		});
 
 	</script> 
+	
+	<script src="js/bootstrap.min.js"></script>
   </body>
 </html>
--- search.php
+++ search.php
@@ -3,38 +3,25 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Safe Journey To Work</title>
-		<!-- logo -->
-		<link rel="shortcut icon" href="images/logo.png" />
-		<!-- css -->
-		<link rel="stylesheet" type="text/css" href="mystyle.css">
-		<!-- Bootstrap -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- Head style -->
+	<?php include("includes/head.html"); ?>  
 		
-		<!--JQuery-->
-	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-		<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+	<!-- <script type="text/javascript">jQuery.noConflict();</script> -->
+	<link rel="stylesheet" type="text/css" href="jquery.autocomplete.css" />
 		
-		<!-- <script type="text/javascript">jQuery.noConflict();</script> -->
-		<link rel="stylesheet" type="text/css" href="jquery.autocomplete.css" />
-		
-		<script>
-			$(document).ready(function(){
-				$("#suburb_id").autocomplete("suburbautocomplete.php", {
-				selectFirst: true
-				});
+	<script>
+		$(document).ready(function(){
+			$("#suburb_id").autocomplete("suburbautocomplete.php", {
+			selectFirst: true
 			});
+		});
 			
-			$(document).ready(function(){				
-				$("#street_id").autocomplete("streetautocomplete.php", {
-				selectFirst: true
-				});
+		$(document).ready(function(){				
+			$("#street_id").autocomplete("streetautocomplete.php", {
+			selectFirst: true
 			});
-		</script>
+		});
+	</script>
 		
 		<style>
 			<!-- Responsive tables firefox -->
@@ -45,53 +32,10 @@
   </head>
   
   <body>
-	<!-- This is the code for the top of window to place the navigation button -->
-    <div class="navbar navbar-default navbar-fix-top" id="topContainer">
-        <div class="container">
-			<div class="navbar-header"> 
-				<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand navbar-left" href="http://www.safejourneytowork.tk/">SafeJourney</a>  
- 			</div>
- 			
- 			<div class="collapse navbar-collapse">
- 			    <ul class="nav navbar-nav">	
- 			        <li><a href="http://www.safejourneytowork.tk/">Home</a></li>
-					<li class="dropdown">
-             			<a id="drop1" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                		Search
-                		<span class="caret"></span></a>
-              			<ul class="dropdown-menu" aria-labelledby="drop1">
-							<li><a href="search.php">By Fine</a></li>
-                			<li><a href="accidentreport.php">By Accident</a></li>
-               				<li role="separator" class="divider"></li>
-                			<li><a href="maptest.html">On Map</a></li>	 
-              			</ul>
-           			</li>			         
- 			        <li id="feedback"><a href="feedback.php">Feedback</a></li>
- 			        <li id="about"><a href="aboutus.php">About Us</a></li>	
- 			    </ul>
-				
- 			    <form class="navbar-form navbar-right">
- 			       	<div class="form-group">
- 			       	   	<input type="email" placeholder="Email" class="form-control" />
- 			        </div>
-  			       	
-  			       	<div class="form-group">
-						<input type="password" placeholder="Password" class="form-control" />
- 			        </div>
- 			        
- 			        <button type="submit" class="btn" id="login">Log In</button>    
-				</form>  		        
- 			</div>
- 	    </div>
-    </div>
+	<!--Navigation bar and style-->
+	<?php include("includes/header.html"); ?>
     
     <div class="container" id="midContainerSearch">
-		
 		<div class="row">
 			<h3 align="center">Search the infringement fine for intersection in Melbourne by entering suburb and street.</h3>
            <!-- offset-3 make the 3 column to the right,move col6 to middle -->    
